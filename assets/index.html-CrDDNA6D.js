a6ed4d89d15797dbeea152e6ee322319fe587539
Sun-ZhenXing <44517244+Sun-ZhenXing@users.noreply.github.com>
2024-01-28 11:21:15 +0000

    Deploying to gh-pages from @ Sun-ZhenXing/vuepress-solid-template@11db43a42558df9b11ba5b4feec917f946ead46b 🚀

---
+++ assets/index.html-CrDDNA6D.js
@@ -0,0 +1,1 @@
+import{_ as s,r as a,o as t,c as n,a as l,b as e}from"./app-b2tlrW9w.js";const r={},c=e("h1",{id:"hello-vuepress",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#hello-vuepress"},[e("span",null,"Hello VuePress")])],-1);function _(d,u){const o=a("AutoCatalog");return t(),n("div",null,[c,l(o)])}const i=s(r,[["render",_],["__file","index.html.vue"]]);export{i as default};
