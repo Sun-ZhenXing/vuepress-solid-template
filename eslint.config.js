a54b89bb970f07c51cc86618a65d34f6fc4c2c28
Sun-ZhenXing <1006925066@qq.com>
2024-08-07 14:18:37 +0800

    chore(deps): update

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,19 @@
 import antfu from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
-import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
 
 const compat = new FlatCompat()
 
 export default antfu(
   {
+    ignores: [
+      '!docs/.vuepress/',
+      'docs/.vuepress/.temp/',
+      'docs/.vuepress/.cache/',
+      'docs/.vuepress/dist/',
+    ],
     markdown: false,
     typescript: true,
   },
-  // sorting various data
-  perfectionistNatural,
   ...compat.config({
     overrides: [{
       extends: ['plugin:markdownlint/recommended'],
